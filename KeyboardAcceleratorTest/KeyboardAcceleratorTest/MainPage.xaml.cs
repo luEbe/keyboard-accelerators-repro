d21c124887c751b3e453d501325d6ff4410a6781
Lukas Eberhardt <lukas.eberhardt@ringana.com>
2020-05-27 10:45:54 +0200

    initial commit

---
+++ KeyboardAcceleratorTest/KeyboardAcceleratorTest/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace KeyboardAcceleratorTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AcceleratorKey = "O";
+            AcceleratorModifiers = "Control,Shift";
+        }
+
+        public string AcceleratorKey { get; set; }
+        public string AcceleratorModifiers { get; set; }
+        private int count = 0;
+        public void ChangeTextBlockText()
+        {
+            textBlock.Text = $"Text changed {++count} times";
+        }
+    }
+}
